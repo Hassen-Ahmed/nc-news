958a3e63d49d80e5f9f3454c39987e19097c237e
Hassen-Ahmed <hassenbest23@gmail.com>
2023-07-18 15:48:47 +0100

    5-View-individual-article update with load spinner

--- src/components/article/SingleArticle.jsx
+++ src/components/article/SingleArticle.jsx
@@ -33,52 +33,49 @@ const SingleArticle = () => {
         });
     }
 
+    if (!singleArticle)
+        return (
+            <div className="loading-container">
+                <p className="loading">Loading ...</p>
+            </div>
+        );
     return (
-        <>
-            {singleArticle ? (
-                <section className="article-comments">
-                    <div className="article__profile--top">
-                        <div className="article__profile--pic">
-                            <img
-                                src={userAvatar}
-                                alt={`profile picture of ${singleArticle.author}`}
-                            />
-                        </div>
+        <section className="article-comments">
+            <div className="article__profile--top">
+                <div className="article__profile--pic">
+                    <img src={userAvatar} alt={`profile picture of ${singleArticle.author}`} />
+                </div>
 
-                        <div>
-                            <h2>{singleArticle.author}</h2>
-                            <p className="article__profile--date">
-                                {date.getDate()}/{date.getMonth()}/{date.getFullYear()}
-                            </p>
-                        </div>
-                    </div>
-                    <p className="article__profile--title">{singleArticle.title}</p>
-                    <div className="article__profile--body-container">
-                        <p className="article__profile--body">
-                            {singleArticle.body.length < 100 || isMore
-                                ? singleArticle.body
-                                : singleArticle.body.slice(0, 100) + "  ..."}
-                        </p>
-                        {!singleArticle.body.length < 100 ? (
-                            <button onClick={setIsMoreHandler}>
-                                show {isMore ? "less" : "more"}
-                            </button>
-                        ) : null}
-                    </div>
+                <div>
+                    <h2>{singleArticle.author}</h2>
+                    <p className="article__profile--date">
+                        {date.getDate()}/{date.getMonth()}/{date.getFullYear()}
+                    </p>
+                </div>
+            </div>
+            <p className="article__profile--title">{singleArticle.title}</p>
+            <div className="article__profile--body-container">
+                <p className="article__profile--body">
+                    {singleArticle.body.length < 100 || isMore
+                        ? singleArticle.body
+                        : singleArticle.body.slice(0, 100) + "  ..."}
+                </p>
+                {!singleArticle.body.length < 100 ? (
+                    <button onClick={setIsMoreHandler}>show {isMore ? "less" : "more"}</button>
+                ) : null}
+            </div>
 
-                    <div className="article__img--container">
-                        <img src={singleArticle.article_img_url} alt={singleArticle.title} />
-                    </div>
-                    <div className="article__profile--bottom">
-                        <div className="article__profile--bottom-1">
-                            <p>{singleArticle.votes}</p>
-                            <BiSolidLike className="article__like-btn" />
-                        </div>
-                        <p>{singleArticle.comments_count} comments</p>
-                    </div>
-                </section>
-            ) : null}
-        </>
+            <div className="article__img--container">
+                <img src={singleArticle.article_img_url} alt={singleArticle.title} />
+            </div>
+            <div className="article__profile--bottom">
+                <div className="article__profile--bottom-1">
+                    <p>{singleArticle.votes}</p>
+                    <BiSolidLike className="article__like-btn" />
+                </div>
+                <p>{singleArticle.comments_count} comments</p>
+            </div>
+        </section>
     );
 };
 
