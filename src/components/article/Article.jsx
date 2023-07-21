18c6d74a334d6e427a2af79820089e4a4eb03450
Hassen-Ahmed <hassenbest23@gmail.com>
2023-07-21 21:26:21 +0100

    comment delete button changed

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -33,7 +33,10 @@ const Article = ({ article, userList }) => {
             <Link to={`/articles/${article.article_id}`} className="article__link">
                 <p className="article__profile--title">{article.title}</p>{" "}
                 <div className="article__img--container">
-                    <img src={article.article_img_url} alt={article.title} />
+                    <img
+                        src={article.article_img_url}
+                        alt={`profile picture of ${article.author}`}
+                    />
                 </div>
             </Link>
         </section>
