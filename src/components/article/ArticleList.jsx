8c9815693a6408c3d70aefb336a11ea77d605e7f
Hassen-Ahmed <hassenbest23@gmail.com>
2023-07-21 15:20:02 +0100

    12-error-handling is done

--- src/components/article/ArticleList.jsx
+++ src/components/article/ArticleList.jsx
@@ -3,16 +3,23 @@ import Article from "./Article";
 import { AiFillHeart } from "react-icons/ai";
 import { NewsDataContext } from "../../data/NewData";
 import { getAllArticles } from "../../utils/api";
+import Error from "../Error";
 
 const ArticleList = ({ topic, sort_by, order }) => {
     const { userList } = useContext(NewsDataContext);
     const [articles, setArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isWrongId, setIsWrongId] = useState(false);
+
     useEffect(() => {
-        getAllArticles(topic, sort_by, order).then((articlesData) => {
-            setArticles(articlesData);
-            setIsLoading(false);
-        });
+        getAllArticles(topic, sort_by, order)
+            .then((articlesData) => {
+                setArticles(articlesData);
+                setIsLoading(false);
+            })
+            .catch(() => {
+                setIsWrongId(true);
+            });
     }, [topic, sort_by, order]);
 
     const loading = (
@@ -27,7 +34,11 @@ const ArticleList = ({ topic, sort_by, order }) => {
     return (
         <>
             {isLoading ? (
-                loading
+                !isWrongId ? (
+                    loading
+                ) : (
+                    <Error msg=" Wrong query!" />
+                )
             ) : (
                 <div className="article-list">
                     {articles.map((article) => {
